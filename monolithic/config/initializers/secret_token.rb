3cfa527484099b1581e4246f33d0c3b9e6dcf522
Robert Gravina <robertg@reallyenglish.com>
2012-10-04 16:14:31 +0900

    initial generated rails app

---
+++ monolithic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monolithic::Application.config.secret_token = '6c516405c85913fa9253e561e512d4c762f50fb55703c7ef61223939b7c0a21e60b456f226505d6515d9a9cf7553af5b45904bebffd39db2b431fc97097bab90'
